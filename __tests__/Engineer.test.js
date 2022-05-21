d4edd91138cf9a86fe9108551c1ac27d05da8527
hvphan <hvphan1993@gmail.com>
2022-05-21 16:33:37 -0700

    added engineer tests that failed and now passed, added engineer.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,24 @@
 const Engineer = require('../lib/Engineer');
 
 // creates Engineer object
+test('creates Engineer object', () => {
+    const engineer = new Engineer('Leo', 1, 'squadleader@turtlepower.com', 'katanatime');
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+// get github from getGithub()
+test('get github', () => {
+    const engineer = new Engineer('Leo', 1, 'squadleader@turtlepower.com', 'katanatime');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// change role to engineer
+test('gets role', () => {
+    const engineer = new Engineer('Leo', 1, 'squadleader@turtlepower.com', 'katanatime');
+
+    expect(engineer.getRole()).toEqual('Engineer');
+});
+
+console.log(new Engineer('Leo', 1, 'squadleader@turtlepower.com', 'katanatime'));
